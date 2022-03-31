5aa996280b6ae6e16f95c06e2545e2eba7b03797
Julian Tam <juliant653@gmail.com>
2022-03-31 15:31:01 -0700

    used mock data to model individual card component

--- client/src/components/Related/components/Related.jsx
+++ client/src/components/Related/components/Related.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Card from './Card.jsx';
+import product from '../../../seedData/productSeed.js';
 
 function Related() {
   return (
     <div>
-      <Card />
+      <h3>Related Products</h3>
+      <Card id={product.id} style={product.idStyles} />
     </div>
   );
 }
