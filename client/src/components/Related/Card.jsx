31b138ace3241a5cbba0d2692acd6c0fe9d3cacf
Julian Tam <juliant653@gmail.com>
2022-03-31 12:28:54 -0700

    worked on fleshing out individual Card component

---
+++ client/src/components/Related/Card.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Card() {
+  return (
+    <div>
+      <button type="button">Click Me</button>
+      <img></img>
+      <div>
+        <dl>
+          <dt>Category</dt>
+          <dd>Name</dd>
+          <dd>Price</dd>
+          <dd>Rating</dd>
+        </dl>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- client/src/components/Related/Related.jsx
+++ client/src/components/Related/Related.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Card from './Card.jsx';
 
 function Related() {
-  return (<>test</>);
+  return (
+    <div>
+      <Card />
+    </div>
+  );
 }
 
 export default Related;
