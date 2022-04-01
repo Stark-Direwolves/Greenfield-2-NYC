e78f369c106843c32cec1ef74286086e98089a6d
kimberlysiu1@gmail.com <kimberlysiu1@gmail.com>
2022-04-01 15:52:42 -0700

    QA route completed, dummy data used to bring down to each component in QA

--- server/routes/qaRoute.js
+++ server/routes/qaRoute.js
@@ -14,8 +14,24 @@ const getQA = (id) => {
   return axios.get(options.url, options);
 };
 
+const getAnswers = (qaId, productId) => {
+  const options = {
+    url: `https://app-hrsei-api.herokuapp.com/api/fec2/rfp/qa/questions/${qaId}/answers?product_id=${productId}`,
+    headers: {
+      Authorization: process.env.GITHUB_AUTH_KEY,
+    },
+  };
+  return axios.get(options.url, options);
+};
+
 router.get('/', (req, res) => {
   getQA(req.query.product_id)
+    .then((results) => res.status(200).send(results.data))
+    .catch((err) => res.status(404).send(err));
+});
+
+router.get('/:question_id/answers', (req, res) => {
+  getAnswers(req.params.question_id, req.query.product_id)
     .then((results) => res.status(200).send(results.data))
     .catch((err) => res.status(404).send(err));
 });
