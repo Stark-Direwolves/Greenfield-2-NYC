13c59c72937fdf0c14321137d38c3c7e33d0f68a
Julian Tam <juliant653@gmail.com>
2022-04-01 11:17:02 -0700

    built products route

--- server/index.js
+++ server/index.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const path = require('path');
-const api = require('./helpers');
+const products = require('./routes/productsRoute');
 require('dotenv').config();
+
 // all keys in .env file available as variables
 // process.env.GITHUB_AUTH_KEY available for requests!
 
 const app = express();
-// const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
-app.get('/:category', (req, res) => {
-  api.get(req.params.category)
-    .then((results) => res.status(200).send(results))
-    .catch((err) => res.status(404).send(err));
-});
+app.use('/products', products);
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
