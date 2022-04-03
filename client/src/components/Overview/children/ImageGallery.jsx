faa12a86d610026d92a5bd03e2801704ec5e94da
Nathaniel Belen <nathaniel.belen@gmail.com>
2022-04-03 10:56:09 -0700

    added functionality to image-selector to run through the array of images

--- client/src/components/Overview/children/ImageGallery.jsx
+++ client/src/components/Overview/children/ImageGallery.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 
 function ImageGallery({ styleImages }) {
+  const limit = styleImages.length - 1;
+  const [currentImageIndex, setCurrentImageIndex] = React.useState(0);
+
+  const goNext = () => {
+    if (currentImageIndex < limit) {
+      setCurrentImageIndex(currentImageIndex + 1);
+    }
+  }
+
+  const goPrevious = () => {
+    if (currentImageIndex > 0) {
+      setCurrentImageIndex(currentImageIndex - 1);
+    }
+  }
+
+  React.useEffect(() => {
+    setCurrentImageIndex(0);
+  }, [styleImages]);
+
   return (
     <div id="image-gallery">
       <div id="image-preview" className="component-separator">
-        <img src={styleImages[0].url} width="670"/>
+        <img src={styleImages[currentImageIndex].url} width="670"/>
       </div>
       <div id="image-selector" className="component-separator">
-        image-selector
+        <button onClick={goPrevious}>previous</button><button onClick={goNext}>next</button>
       </div>
     </div>
   );
