f13635c83e072165085898ca160bf40413f0423f
Patrick <ptriklee@gmail.com>
2022-04-06 09:41:02 -0700

    Router get request, dealing w/ api information now, put request halfway implemented

--- client/src/seedData/reviewSeed.js
+++ client/src/seedData/reviewSeed.js
@@ -16,7 +16,7 @@ const list = {
       photos: [
         {
           id: 2259011,
-          url: 'http://vancouvereast.vet/wp-content/uploads/2018/01/Heavy-Breathing-Cat.jpg',
+          url: 'https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80',
         },
       ],
     },
