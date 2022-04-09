2afb9f6b10b37bcdcda32bc6784cb31612ddb353
Julian Tam <juliant653@gmail.com>
2022-04-08 17:25:57 -0700

    installed prop-types

--- client/src/components/Related/components/AddOutfit.jsx
+++ client/src/components/Related/components/AddOutfit.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { PlusIcon } from '@heroicons/react/outline';
 import AddOutfitContainer from '../styles/AddOutfitContainer.styled';
 import { AddButton } from '../styles/StyledButtons.styled';
