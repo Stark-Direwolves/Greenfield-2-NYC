f7f5b47b8520aea94320e495d3d57e604a5eb296
Julian Tam <juliant653@gmail.com>
2022-04-13 15:34:31 -0700

    more css styling

--- client/src/components/Related/styles/Image.styled.js
+++ client/src/components/Related/styles/Image.styled.js
@@ -4,7 +4,8 @@ const ImageContainer = styled.div`
   width: 250px;
   height: 250px;
   border: 1px solid black;
-  box-shadow: 2px 0 5px gray;
+  box-shadow: 2px 2px 5px gray;
+  z-index: 2;
 `;
 
 const StyledImage = styled.img`
