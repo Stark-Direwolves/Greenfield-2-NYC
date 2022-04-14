83c72817136a17e900f06c65ba64d945b907636e
kimberlysiu1@gmail.com <kimberlysiu1@gmail.com>
2022-04-13 19:18:28 -0700

    Updated search style

--- client/src/components/QA/components/styles/Search.style.jsx
+++ client/src/components/QA/components/styles/Search.style.jsx
@@ -1,36 +1,33 @@
 import styled from 'styled-components';
 
 export const SContainer = styled.div`
-  background: #fff;
-  height: 30px;
-  border-radius: 30px;
-  padding: 10px 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  transition: 0.8s;
-box-shadow:  4px 4px 6px 0 rgba(255,255,255,.3),
-            -4px -4px 6px 0 rgba(116, 125, 136, .2),
-  inset -4px -4px 6px 0 rgba(255,255,255,.2),
-  inset 4px 4px 6px 0 rgba(0, 0, 0, .2);
-  margin-bottom: 18.72px;
+    width: 50%;
+    appearance: none;
+    outline: none;
+    height: 48px;
+    font-size: 16px;
+    letter-spacing: 0.5px;
+    font-weight: 700;
+    background: rgb(245, 245, 245);
+    border-bottom: 2px solid rgb(33, 42, 47);
+    border-top-width: 0px;
+    border-right-width: 0px;
+    border-left-width: 0px;
+    border-top-style: solid;
+    border-right-style: solid;
+    border-left-style: solid;
+    border-radius: 0px;
+    padding: 2px 12px 0px;
+    color: rgb(33, 42, 47);
+    box-shadow: transparent 0px 2px 0px 0px inset,
 `;
 
 export const Input = styled.input`
-  background: transparent;
-  border: none;
-  outline:none;
-  width: 100%;
-  font-weight: 500;
-  font-size: 16px;
-  transition: 0.8s;
+background: transparent;
+/* border: none; */
+outline: none;
+width: 100%;
+font-weight: 500;
+font-size: 16px;
+transition: 0.8s;
   `;
-
-// export const Form = styled.form`
-//     display: flex;
-//     flex-grow: 1;
-//     flex-shrink: 0;
-//     flex-basis: 260px;
-//     margin-right: 20px;
-//   `;
