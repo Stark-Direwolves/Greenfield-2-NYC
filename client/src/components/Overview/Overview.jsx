f9192ec69a0cf675c2fdb3258fc2d19003ca61e4
Julian Tam <juliant653@gmail.com>
2022-04-16 15:46:04 -0700

    did some linting stuff on overview

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable react/forbid-prop-types */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import AddToCart from './components/AddToCart';
@@ -27,68 +25,71 @@ function Overview({
   expanded,
   setExpanded,
 }) {
-  // const [expanded, setExpanded] = React.useState(false);
-
   const [curStyles, setCurStyles] = React.useState(styles.results);
 
-  React.useEffect(() => { // if product is 65632, hardcode null images. is there a better way?
+  React.useEffect(() => {
     if (product.id === 65632) {
-      const replacement = Object.create(styles.results)
-      replacement[0].photos[0].url = '/assets/blackblack.jpg'
-      replacement[0].photos[0].thumbnail_url = '/assets/blackblack.jpg'
-      replacement[1].photos[0].url = '/assets/blackgold.jpg'
-      replacement[1].photos[0].thumbnail_url = '/assets/blackgold.jpg'
-      replacement[2].photos[0].url = '/assets/goldblack.jpg'
-      replacement[2].photos[0].thumbnail_url = '/assets/goldblack.jpg'
-      replacement[3].photos[0].url = '/assets/goldgold.jpg'
-      replacement[3].photos[0].thumbnail_url = '/assets/goldgold.jpg'
+      const replacement = Object.create(styles.results);
+      replacement[0].photos[0].url = '/assets/blackblack.jpg';
+      replacement[0].photos[0].thumbnail_url = '/assets/blackblack.jpg';
+      replacement[1].photos[0].url = '/assets/blackgold.jpg';
+      replacement[1].photos[0].thumbnail_url = '/assets/blackgold.jpg';
+      replacement[2].photos[0].url = '/assets/goldblack.jpg';
+      replacement[2].photos[0].thumbnail_url = '/assets/goldblack.jpg';
+      replacement[3].photos[0].url = '/assets/goldgold.jpg';
+      replacement[3].photos[0].thumbnail_url = '/assets/goldgold.jpg';
       setCurStyles(replacement);
     }
   }, []);
 
   return (
-    <StyledOverview expanded={expanded} >
-      <ImageGallery styleImages={currentStyle.photos} setExpanded={setExpanded} expanded={expanded} />
-      { !expanded ?
-        <StyledDetails>
-          <ProductInformation
-            currentProduct={product}
-            currentStyle={currentStyle}
-            meta={meta}
-          />
-          <StyleSelector
-            currentStyle={currentStyle}
-            setCurrentStyle={setCurrentStyle}
-            styles={curStyles}
-          />
-          <AddToCart
-            currentStyle={currentStyle}
-            styles={curStyles}
-            currentSku={currentSku}
-            currentSize={currentSize}
-            currentQty={currentQty}
-            setCurrentSku={setCurrentSku}
-            setCurrentSize={setCurrentSize}
-            setCurrentQty={setCurrentQty}
-            currentTotal={currentTotal}
-            setCurrentTotal={setCurrentTotal}
-          />
-          <SocialMedia>
-            <img src="https://img.icons8.com/ios/50/000000/twitter--v1.png" alt="twitter" />
-            <img src="https://img.icons8.com/ios/50/000000/facebook-new.png" alt="facebook" />
-            <img src="https://img.icons8.com/ios/50/000000/pinterest--v1.png" alt="pinterest" />
-          </SocialMedia>
-        </StyledDetails> : <></>
-      }
+    <StyledOverview expanded={expanded}>
+      <ImageGallery
+        styleImages={currentStyle.photos}
+        setExpanded={setExpanded}
+        expanded={expanded}
+      />
+      { !expanded
+        ? (
+          <StyledDetails>
+            <ProductInformation
+              currentProduct={product}
+              currentStyle={currentStyle}
+              meta={meta}
+            />
+            <StyleSelector
+              currentStyle={currentStyle}
+              setCurrentStyle={setCurrentStyle}
+              styles={curStyles}
+            />
+            <AddToCart
+              currentStyle={currentStyle}
+              styles={curStyles}
+              currentSku={currentSku}
+              currentSize={currentSize}
+              currentQty={currentQty}
+              setCurrentSku={setCurrentSku}
+              setCurrentSize={setCurrentSize}
+              setCurrentQty={setCurrentQty}
+              currentTotal={currentTotal}
+              setCurrentTotal={setCurrentTotal}
+            />
+            <SocialMedia>
+              <img src="https://img.icons8.com/ios/50/000000/twitter--v1.png" alt="twitter" />
+              <img src="https://img.icons8.com/ios/50/000000/facebook-new.png" alt="facebook" />
+              <img src="https://img.icons8.com/ios/50/000000/pinterest--v1.png" alt="pinterest" />
+            </SocialMedia>
+          </StyledDetails>
+        ) : null}
     </StyledOverview>
   );
 }
 
 Overview.propTypes = {
-  product: PropTypes.object,
-  styles: PropTypes.object,
-  meta: PropTypes.object,
-  currentStyle: PropTypes.object,
+  product: PropTypes.instanceOf(Object),
+  styles: PropTypes.instanceOf(Object),
+  meta: PropTypes.instanceOf(Object),
+  currentStyle: PropTypes.instanceOf(Object),
   currentSku: PropTypes.string,
   currentSize: PropTypes.string,
   currentQty: PropTypes.string,
@@ -98,6 +99,26 @@ Overview.propTypes = {
   setCurrentQty: PropTypes.func,
   setCurrentTotal: PropTypes.func,
   setCurrentSize: PropTypes.func,
+  expanded: PropTypes.bool,
+  setExpanded: PropTypes.func,
+};
+
+Overview.defaultProps = {
+  product: {},
+  styles: {},
+  meta: {},
+  currentStyle: {},
+  currentSku: '',
+  currentSize: '',
+  currentQty: '',
+  currentTotal: 0,
+  setCurrentStyle: () => {},
+  setCurrentSku: () => {},
+  setCurrentQty: () => {},
+  setCurrentTotal: () => {},
+  setCurrentSize: () => {},
+  expanded: false,
+  setExpanded: () => {},
 };
 
 export default Overview;
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const CompressionPlugin = require("compression-webpack-plugin");
+const CompressionPlugin = require('compression-webpack-plugin');
 const zlib = require('zlib');
 
 module.exports = {
@@ -30,15 +30,15 @@ module.exports = {
   },
   plugins: [
     new CompressionPlugin({
-      filename: "[path][base].gz",
-      algorithm: "gzip",
+      filename: '[path][base].gz',
+      algorithm: 'gzip',
       test: /\.js$|\.css$|\.html$/,
       threshold: 10240,
       minRatio: 0.8,
     }),
     new CompressionPlugin({
-      filename: "[path][base].br",
-      algorithm: "brotliCompress",
+      filename: '[path][base].br',
+      algorithm: 'brotliCompress',
       test: /\.(js|css|html|svg)$/,
       compressionOptions: {
         params: {
